20948e966275cb21b7be20ae9eb37506dd2587da
Joe Borg <jdb@sitrep3.com>
2013-09-24 21:37:57 +0100

    Fixing indents

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ class PyPing(object):
 	"""
 	Send pings and write the results like
 	a Cisco router.
-    """
+	"""
 	def __init__(self, args):
 		"""
 		Move the arguments into the instance's
 		namespace.
-        """
+		"""
 		self.destination = args.destination
 
 	def __call__(self):
 		"""
 		Run the ping, print '!' for success
 		and '.' for failure.
-        """
+		"""
 		p = Ping(self.destination)
 		if p.do():
 			print "!"
@@ -27,7 +27,7 @@ class PyPing(object):
 def main():
 	"""
 	Parse the arguments.
-    """
+	"""
 	parser = argparse.ArgumentParser()
 	parser.add_argument('destination')
 
